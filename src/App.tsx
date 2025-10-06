5117dd3917f7ce978d92e14d2644eb4aca539d79
TorqueReborn <torquereborn@gmail.com>
2025-10-06 08:12:40 +0530

    Create display layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
+
 const App = () => {
+  const [currentValue, setCurrentValue] = useState<number>(0)
+
   return (
-    <div>App</div>
+    <div className="flex items-center justify-center h-screen">
+      <div className="container bg-gray-800 rounded w-[25%] h-[75%]">
+        <div className="w-full bg-white text-black text-right px-3 py-2 text-xl font-bold">
+          {currentValue}
+        </div>
+      </div>
+    </div>
   )
 }
 
